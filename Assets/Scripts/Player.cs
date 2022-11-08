d964b7d4de05fc45a072d14c2aa0a3a4e1f8d805
JinSeong970419 <73704533+JinSeong970419@users.noreply.github.com>
2022-11-08 14:19:20 +0900

    InteractableMap 설정
    농사 가능 구역 설정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,21 @@ public class Player : MonoBehaviour
         inventory = new Inventory(21);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3Int position = new Vector3Int((int)transform.position.x, (int)transform.position.y, 0);
+
+            if (GameManager.instance.tileManager.IsInteractable(position))
+            {
+                Debug.Log("사용 가능 타일");
+                GameManager.instance.tileManager.SetInteracted(position);
+            }
+        }
+    }
+
+    // 아이템 버리기
     public void DropItem(Collectable item)
     {
         Vector3 spawnLocation = transform.position;
